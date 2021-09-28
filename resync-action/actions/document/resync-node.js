67660c0838530e9733298f26e2d964a0f582072d
mwhitman <mwhitman95@gmail.com>
2021-09-28 09:47:35 -0400

    resync module wip

---
+++ resync-action/actions/document/resync-node.js
@@ -0,0 +1,43 @@
+define(function(require, exports, module) {
+
+    var Ratchet = require("ratchet/web");
+    var Actions = require("ratchet/actions");
+    var $ = require("jquery");
+
+    var OneTeam = require("oneteam");
+
+    return Actions.register("resync-node", Ratchet.AbstractAction.extend({
+
+        defaultConfiguration: function()
+        {
+            var config = this.base();
+
+            config.title = "Resync node";
+            config.iconClass = "glyphicon glyphicon-repeat";
+
+            return config;
+        },
+
+        execute: function(config, actionContext, callback)
+        {
+            var self = this;
+            var ratchet = actionContext.ratchet;
+
+            var project = actionContext.observable("project").get();
+            var document = actionContext.observable("document").get();
+            actionContext.listOfAttachmentIds = [];
+
+            // target live state
+            var state = "live";
+            var nodeIds = [document._doc];
+            OneTeam.pleaseWait("Resyncing...");
+            Chain(document).getDriver().gitanaPost(document.getUri() + "/publishing/resync", {}, {"state": state, "nodeIds": nodeIds}).then(function () {
+                OneTeam.pleaseWaitDone();
+                callback();
+            });
+        }
+
+    }));
+
+});
+
